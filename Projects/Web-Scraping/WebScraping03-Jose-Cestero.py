db4b97a1e5715b4144f7adc6d5340edddb98b3e7
CSI-Jose-Cestero <22-021@sanignacio.pr>
2022-03-29 08:53:20 -0400

    WebScraping03

---
+++ Projects/Web-Scraping/WebScraping03-Jose-Cestero.py
@@ -0,0 +1,8 @@
+import requests
+import bs4
+
+res = requests.get('https://www.sanignacio.pr/sobre-csi/historia-del-colegio')
+res.raise_for_status()
+SanIgnaciopr = bs4.BeautifulSoup(res.text, 'html.parser')
+elems = SanIgnaciopr.select('p')
+print(elems[2].getText() + " " + elems[3].getText())
